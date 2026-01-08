209e0273056698918985d150ba1ae77910157d86
Anthorinco <anthonicarvalho30@gmail.com>
2026-01-08 20:38:03 -0300

    ...

---
+++ Exercises/Projects/Cap 2/1./main.c
@@ -0,0 +1,24 @@
+/*
+*Objective: Make a picture shown on the book in C.
+*Author [Anthoni]
+* 01/08/2026
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("         *\n");
+    printf("        *\n");
+    printf("       *\n");
+    printf(" *    *\n");
+    printf("  *  *\n");
+    printf("    *\n");
+    
+
+
+
+
+
+
+    return 0;
+}
