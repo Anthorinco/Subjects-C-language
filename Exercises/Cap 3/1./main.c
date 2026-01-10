b64e3a175a62239c8ffbf58d39084b22e7b0fcc8
Anthorinco <anthonicarvalho30@gmail.com>
2026-01-09 22:44:25 -0300

    ....

---
+++ Exercises/Cap 3/1./main.c
@@ -0,0 +1,18 @@
+/*
+*Objective: See the outputs of the // Exercise below.
+*Author [Anthoni]
+* 01/09/2026
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Exercise
+    printf("%6d, %4d\n", 86, 1040);
+    printf("%12.5e\n", 30.253);
+    printf("%.4f\n", 83.162);
+    printf("%-6.2g\n", .0000009979);
+
+}
